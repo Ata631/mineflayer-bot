a7a7553aa8beb2b7c783e1475074168fd2f4e9d6
Ata631 <atademetgungor@gmail.com>
2025-12-06 12:37:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mineflayer = require("mineflayer");
 
 const bot = mineflayer.createBot({
-  host: "dynamic-7.magmanode.com", // Ör: play.example.com
-  port: 25680, // Değiştirme! Sunucun farklıysa değiştir
+  host: "emerald.magmanode.com", // Ör: play.example.com
+  port: 28683, // Değiştirme! Sunucun farklıysa değiştir
   username: "Botmusunolum", // Premium değilse herhangi bir isim
   version: false,
 });
