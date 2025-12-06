17ff874708c4e598be316331c543a35a933e12e2
Ata631 <atademetgungor@gmail.com>
2025-12-06 19:35:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,35 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Bot aktif!'));
 app.listen(3000, () => console.log('Ping server çalışıyor.'));
+const mineflayer = require("mineflayer");
 
+function startBot() {
+  const bot = mineflayer.createBot({
+    host: "emerald.magmanode.com",
+    port: 28683,
+    username: "Botmusunolum",
+    version: false,
+  });
+
+  bot.on("spawn", () => {
+    console.log("Bot sunucuya bağlandı!");
+  });
+
+  bot.on("end", () => {
+    console.log("Bot düştü, yeniden bağlanıyor...");
+    setTimeout(startBot, 5000);
+  });
+
+  bot.on("kicked", console.log);
+  bot.on("error", console.log);
+
+  // AFK koruması
+  setInterval(() => {
+    bot.setControlState("jump", true);
+    setTimeout(() => bot.setControlState("jump", false), 250);
+  }, 15000);
+
+  return bot;
+}
+
+startBot();
