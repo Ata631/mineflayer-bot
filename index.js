884d9d086ad175531aaffd47cb7f004baf1f99b9
Ata631 <atademetgungor@gmail.com>
2025-12-06 12:15:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ bot.on("end", () => {
   console.log("Bot bağlantısı koptu. Yeniden bağlanıyor...");
   setTimeout(() => process.exit(), 5000);
 });
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot aktif!'));
+app.listen(3000, () => console.log('Ping server çalışıyor.'));
+
